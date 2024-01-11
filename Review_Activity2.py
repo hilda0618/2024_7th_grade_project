ba89c3dee8fc9df8b12ca8fc84dfc4bcb676089f
hilda0618 <hiliu@ucsd.edu>
2024-01-10 22:22:54 -0800

    Starter code for review activity 2

---
+++ Review_Activity2.py
@@ -0,0 +1,21 @@
+#Your First and Last Name
+#7th Grade Computer Science
+#Python Review Activity Part 2
+#TODAY's DATE
+
+#Write a program to find the difference between a Sphere's volume and Surface Area given a radius of 3.
+#Hint: Answer should be zero
+#Hint: Use 3.14 for pi
+#Hint: Use x**y and pow(x,y) for x to the power of y
+#Hint: the formula for volume is 4/3 * pi * r^3 
+#Hint: the formula for surface area is 4 * pi * r^2
+#Hint: use round(num) to round value
+def exercise1():
+  #Your code starts here
+    
+#Write a program to calculate rabbit population. Every year the population of rabbits on an island increases by 35%. This year there are 200 rabbits on the isalnd. How many will be there after 3, 5 and 10 years?
+def exercise2():
+  #Your code starts here
+
+exercise1()
+exercise2()
